30e04afc1685b5b22092c192d06a01cf9397aee2
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-02-23 06:20:15 -0700

    fix: apply changes to work with CrewAI 0.14.0
    Also adds some more tasks to test against latest tools.

--- run_crew.py
+++ run_crew.py
@@ -1,24 +1,46 @@
 from crewai import Crew, Process, Task
 from agents import BitcoinCrew
 
+# set global vars
+from dotenv import load_dotenv
+from langchain.globals import set_debug
+
+load_dotenv()
+set_debug(True)
+
+# create a default language model
+import os
+from langchain_openai import ChatOpenAI
+
+default_llm = ChatOpenAI(
+    model=os.environ.get("OPENAI_MODEL_NAME"),
+)
 
 def engage_crew_with_tasks():
     # define agent
     wallet_agent = BitcoinCrew.wallet_agent()
     # define the tasks
     task_1 = Task(
-        description="Get the addresses of the configured wallet.",
+        description="What wallet addresses do you have access to?",
         agent=wallet_agent,
     )
     task_2 = Task(
-        description="Get information about the configured wallet.",
+        description="What is the fourth address you have access to?",
+        agent=wallet_agent,
+    )
+    task_3 = Task(
+        description="What information do you know about the currently configured wallet?",
+        agent=wallet_agent,
+    )
+    task_4 = Task(
+        description="What is the aiBTC balance for your currently configured wallet?",
         agent=wallet_agent,
     )
     # create a crew
     wallet_crew = Crew(
         agents=[wallet_agent],
         process=Process.sequential,
-        tasks=[task_1, task_2],
+        tasks=[task_1, task_2, task_3, task_4],
         verbose=True,
     )
     # run the crew
