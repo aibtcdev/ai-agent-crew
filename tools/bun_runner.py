618603ef4ef6f7ad02f5fa4fbc003355fe731ff6
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-02-29 07:20:22 -0700

    fix: update runner to accept positional arg

--- tools/bun_runner.py
+++ tools/bun_runner.py
@@ -1,19 +1,22 @@
 import subprocess
 
-
 class BunScriptRunner:
     working_dir = "./scripts/"
     script_dir = "src"
 
     @staticmethod
-    def bun_run(contract_name: str, script_name: str):
-        """Runs a typescript script using bun"""
-        # command to run
+    def bun_run(contract_name: str, script_name: str, arg: str = None):
+        """Runs a TypeScript script using bun with an optional positional argument."""
         command = [
             "bun",
             "run",
             f"{BunScriptRunner.script_dir}/{contract_name}/{script_name}",
         ]
+
+        # Append the optional argument if provided
+        if arg is not None:
+            command.append(arg)
+
         try:
             result = subprocess.run(
                 command,
@@ -24,5 +27,4 @@ class BunScriptRunner:
             )
             return {"output": result.stdout, "error": None, "success": True}
         except subprocess.CalledProcessError as e:
-            # If the subprocess call failed, return the error and a failure flag
             return {"output": None, "error": e.stderr, "success": False}
