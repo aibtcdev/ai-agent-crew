5084c6fdf754e49dac87c8685434ecaea66b816c
hashhavoccat <162091348+hashhavoccat@users.noreply.github.com>
2024-08-28 20:05:08 -0700

    temporary switch the execution tab

--- aibtc-v1/components/execution_tab.py
+++ aibtc-v1/components/execution_tab.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from crews.smart_contract_audit_crew import AIBTC_Crew
+from crews.wallet_summary_crew import AIBTC_Crew
 
 
 def render_execution_tab():
     # st.write("This is where the magic happens.")
-    AIBTC_Crew.render_smart_contract_analysis_crew()
+    AIBTC_Crew.render_wallet_summary_crew()
