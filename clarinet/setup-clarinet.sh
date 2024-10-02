9b07e61f63d87ff9c2dcb06ea163078c8219c456
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-10-02 16:56:00 -0700

    fix: update clarinet version and setup steps
    This should work better if someone runs the setup script from a local machine for some reason.

--- clarinet/setup-clarinet.sh
+++ clarinet/setup-clarinet.sh
@@ -38,12 +38,18 @@ mkdir -p "$CLARINET_SETUP_DIR"
 mkdir -p "$CLARINET_BIN_DIR"
 mkdir -p "$CLARINET_DEPS_DIR"
 
-# download and extract clarinet 2.8.0
-curl -L -o "clarinet.tar.gz" "https://github.com/hirosystems/clarinet/releases/download/v2.8.0/clarinet-linux-x64-glibc.tar.gz"
+# download and extract clarinet 2.9.0
+curl -L -o "clarinet.tar.gz" "https://github.com/hirosystems/clarinet/releases/download/v2.9.0/clarinet-linux-x64-glibc.tar.gz"
 tar -xzf "clarinet.tar.gz" -C "$CLARINET_BIN_DIR"
 rm "clarinet.tar.gz"
 chmod +x "$CLARINET_BIN_DIR/clarinet"
 echo "Clarinet installed to $CLARINET_BIN_DIR/clarinet"
+
+# check if REPL_ID env var is set
+if [ -z "$REPL_ID" ]; then
+    echo "REPL_ID environment variable not set. Skipping Clarinet patching."
+    exit 0
+fi
 
 # patch clarinet to use glibc 2.34
 # replit defaults to the newer 2.39
@@ -56,44 +62,3 @@ if command -v patchelf >/dev/null 2>&1; then
 else
     echo "patchelf not found. Skipping Clarinet patching."
 fi
-
-# create configuration file
-cat > "$CLARINET_CONFIG_FILE" << EOL
-#!/bin/bash
-export CLARINET_SETUP_DIR="$CLARINET_SETUP_DIR"
-export CLARINET_BIN_DIR="$CLARINET_BIN_DIR"
-export CLARINET_DEPS_DIR="$CLARINET_DEPS_DIR"
-export LD_LIBRARY_PATH="$CLARINET_DEPS_DIR:/usr/lib/x86_64-linux-gnu:\$LD_LIBRARY_PATH"
-export PATH="$CLARINET_BIN_DIR:\$PATH"
-EOL
-echo "Configuration file created at $CLARINET_CONFIG_FILE"
-
-# create a run script
-cat > "$CLARINET_SETUP_DIR/run-clarinet.sh" << EOL
-#!/bin/bash
-source "$CLARINET_CONFIG_FILE"
-clarinet "\$@"
-EOL
-chmod +x "$CLARINET_SETUP_DIR/run-clarinet.sh"
-echo "Run script created at $CLARINET_SETUP_DIR/run-clarinet.sh"
-
-# uncomment to print debug information
-#echo "1: Custom paths set by script:"
-#echo "  CLARINET_SETUP_DIR=$CLARINET_SETUP_DIR"
-#echo "  CLARINET_BIN_DIR=$CLARINET_BIN_DIR"
-#echo "  CLARINET_DEPS_DIR=$CLARINET_DEPS_DIR"
-#echo "  CLARINET_CONFIG_FILE=$CLARINET_CONFIG_FILE"
-#echo "2. Custom GLIBC version for Clarinet:"
-#"$CLARINET_DEPS_DIR/ld-linux-x86-64.so.2" --version
-#echo "3. Clarinet binary information:"
-#file "$CLARINET_BIN_DIR/clarinet"
-#echo "4. Clarinet library dependencies:"
-#LD_LIBRARY_PATH="$CLARINET_DEPS_DIR:/usr/lib/x86_64-linux-gnu:\$LD_LIBRARY_PATH" ldd "$CLARINET_BIN_DIR/clarinet"
-#echo "5. Attempting to run Clarinet:"
-#clarinet --version
-#echo "6. Usage:"
-#echo "source $CLARINET_CONFIG_FILE && clarinet --version"
-
-echo "Clarinet setup complete!"
-echo "To use Clarinet, run: source $CLARINET_CONFIG_FILE && clarinet [command]"
-echo "Or use the run script: $CLARINET_SETUP_DIR/run-clarinet.sh [command]"
